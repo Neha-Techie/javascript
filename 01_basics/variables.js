852e0d79f4c8bc594f1d274a4bfcd3b6e5eec05c
Neha Teli <66867083+Neha-Techie@users.noreply.github.com>
2025-04-27 14:37:48 +0530

    Create variables.js
    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountID = 14555 //it is just a number so no double qoutes
+let accountEmail = "neha@gmail.com"
+var accountPassword = "12345" //here the 12345 is a number but it is a string that's why it is in double qoutes because passwords can be alphanumeric/numeric/alphbets so
+accountCity = "Jaipur" 
+// let's try change everything bcoz const stays same var changes
+
+accountID = 2
+console.log(accountID);
+
